7442e41e753c80be545497684ea01a89357ed01f
necrygor <berkay.avsar@protonmail.com>
2022-04-04 23:14:16 +0300

    declaration and little work towards worker

--- include/Worker.h
+++ include/Worker.h
@@ -5,9 +5,60 @@
 #ifndef POSTGRES_CSV_PARSER_WORKER_H
 #define POSTGRES_CSV_PARSER_WORKER_H
 
+#include <string>
+#include <vector>
+#include <map>
+
 
 class Worker {
+/*
+ * Check if storage is populated
+ * Read data from shared storage
+ * Generate HTTP request
+ * send to graylog server
+ */
 
+public:
+    explicit Worker(const std::string &host, const std::string& remote, const std::string &port);
+
+    /*
+     * Entry point for the worker
+     */
+    void run();
+
+private:
+    /*
+     * Check if storage is populated
+     * @param const std::vector<std::map<std::string, std::string>> &storage
+     * @return bool
+     */
+    bool check_storage(const std::vector<std::map<std::string, std::string>> &storage) const;
+
+    /*
+     * Read data from shared storage
+     * @param const std::vector<std::map<std::string, std::string>> &storage
+     */
+    void read_data(const std::vector<std::map<std::string, std::string>> &storage) const;
+
+    /*
+     * Generate HTTP request
+     * @param const std::map<std::string, std::string> &data
+     * @return std::string request string
+     */
+    std::string generate_request(std::map<std::string, std::string> &data) const;
+
+    /*
+     * send to graylog server
+     * @param const std::string &request
+     */
+    void send_request(const std::string&);
+
+
+    static int request_generated;
+    static int request_sent;
+
+    std::string request_url;
+    std::string hostname;
 };
 
 
--- src/Worker.cpp
+++ src/Worker.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "Worker.h"
+
+Worker::Worker(const std::string &host, const std::string &remote, const std::string &port) : hostname(host), request_url(remote + ":" + port) {
+
+}
+
+void Worker::run() {
+
+}
+
+bool Worker::check_storage(const std::vector<std::map<std::string, std::string>> &storage) const {
+    return !storage.empty();
+}
+
+void Worker::read_data(const std::vector<std::map<std::string, std::string>> &storage) const {
+
+}
+
+std::string Worker::generate_request(std::map<std::string, std::string> &data) const {
+    std::string request{R"({"version": "1.1", "host": ")"};
+    request += hostname;
+    request += "\", ";
+    for (auto &item : data) {
+        std::string key = item.first;
+        std::string value = item.second;
+        request += key + "\": \"" + value + "\"";
+    }
+
+    return request;
+}
+
+void Worker::send_request(const std::string &) {
+
+}
